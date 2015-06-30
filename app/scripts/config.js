5fcb78ec9f0969d5151e52041ed6d29f89412034
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-06-30 09:54:34 -0300

    Modify readme

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 /* jshint quotmark:true, indent:false, white: false */
-// This file was generated at build time on Tue Jun 30 2015 09:45:39 GMT-0300 (ART)
+// This file was generated at build time on Tue Jun 30 2015 09:53:11 GMT-0300 (ART)
 // Any change here will be lost
 
 'use strict';
