43b8000738ddd35c19a05289cdd9850f2f203bfc
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-07-06 17:23:55 -0300

    add jshintrc

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 /* jshint quotmark:true, indent:false, white: false */
-// This file was generated at build time on Thu Jul 02 2015 00:48:13 GMT-0300 (ART)
+// This file was generated at build time on Mon Jul 06 2015 16:36:47 GMT-0300 (ART)
 // Any change here will be lost
 
 'use strict';
