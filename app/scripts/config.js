95032a51139276ee6aef478f721f0de94567f4f6
nicov <n.villalonga@globant.com>
2015-07-10 13:33:21 -0300

    fix merge conflicts

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,7 @@
 /* jshint quotmark:true, indent:false, white: false */
+
 // This file was generated at build time on Wed Jul 08 2015 14:19:43 GMT-0300 (ART)
+
 // Any change here will be lost
 
 'use strict';
