c6460a4bb85954feb8d13e780343aec3fc2805ef
unknown <n.villalonga@AR-IT04716.globant.com>
2015-07-16 14:44:35 -0300

    displaying one rectangle flight

--- app/scripts/directives/schedule.js
+++ app/scripts/directives/schedule.js
@@ -10,48 +10,50 @@ angular.module('Directives')
     		scope: {
     				flight: '='
     			},
-    		link: function(scope, elem) {
+    		link: function(scope) {
                 //get d3 context
     			var d3 = $window.d3,
-                    //get the flight object from ng-repeat
-                    flight = scope.flight;
+                    //get the flight object from ng-repeat.. data should be in array format
+                    flight = [];
 
-                console.log('flightTime ' + flight.flightTime);
-                //console.log(attrs);
+                flight.push({'flightTime': scope.flight.flightTime});
 
                 //function to draw rectangles in the view
                 var drawRect = function() {
                         //get the svg fom the view
-        				var svgContainer = d3.select('svg'),
+                        //var svgContainer = d3.select('svg'),
+                        var svgContainer = d3.select('#sched'),
                             //get the rectangle figures (yet not created)
                             rects = svgContainer.selectAll('rect')
                                         //attach the data to the rectangles, now they will be created from the data
-                                        .data(scope.flight)
+                                        .data(flight)
                                         .enter()
                                         //append the rectangles with the __data__ to svg
-                                        .append('rect'),
+                                        .append('rect')
                             //set rectangles attributes
-                            rectsAttrs = rects
+                            //rectsAttrs = rects
                                             .attr('height', 80)
                                             //give the rectangle a dynamic width depending on the flightTime
-                                            .attr('width', function(d){
-                                                console.log(d);
+                                            .attr('width', 80); /* this anonymous function should set width value
+                                            dynamically based on __data__ (d3 data bound), but is called only once
+                                            function(d){
+                                                console.log('d');
                                                 return d.flightTime;
-                                            });
+                                            });*/
 
                         //just avoiding jshint commit error.. declared but never used
-                        rectsAttrs = rectsAttrs;
+                        rects = rects;
+
+                        console.log(svgContainer);
                     };
-    		
+            
                 //scope.$watch('flight.flightTime', function(newVal, oldVal) {
-    			scope.$watch('flight.flightTime', function(newVal) {
-    				if(!newVal) {
-    					return;
+                scope.$watch('flight.flightTime', function(newVal) {
+                    if(!newVal) {
+                        return;
                     }
 
                     drawRect();
-                    console.log(elem);
-                    //console.log(elem[0].childNodes[0].childNodes[0].__data__);
     			});
     		}
     	};
